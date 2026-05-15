1bd333d60e320c3ca41c9099e1f1ab1c2670d45c
mustaqinIshak <mustaqinishak@gmail.com>
2026-05-15 17:24:28 +0800

    testing security

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,16 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  //untuk mengatasi masalah CORS saat pengembangan, 
+  // Anda dapat menambahkan konfigurasi proxy di bawah ini. 
+  // Pastikan untuk menyesuaikan target dengan URL backend Anda.  
+  // server: {
+  //   proxy: {
+  //     '/api': {
+  //       target: 'http://localhost:8000',
+  //       changeOrigin: true,
+  //       secure: false,
+  //     },
+  //   },
+  // },
 })
